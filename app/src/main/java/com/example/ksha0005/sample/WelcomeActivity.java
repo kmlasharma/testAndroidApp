4ca13b5f864637d3fd2ced8f5c35d1386c073b56
“Kmla <kmla.sharma@synchronoss.com>
2016-06-21 17:46:10 +0100

    Added new list view

--- app/src/main/java/com/example/ksha0005/sample/WelcomeActivity.java
+++ app/src/main/java/com/example/ksha0005/sample/WelcomeActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 /**
  * Created by ksha0005 on 20/06/2016.
  */
@@ -13,10 +16,21 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_message);
+        setUpListView();
     }
 
     public void logOut(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void setUpListView() {
+        String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X", "Windows7","Max OS X"
+        , "Windows7","Max OS X", "Windows7","Max OS X", "Windows7","Max OS X"};
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
     }
 }
