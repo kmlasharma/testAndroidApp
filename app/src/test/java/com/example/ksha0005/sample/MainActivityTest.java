b6dfef466146c25502d9e2f7ec9a57eeebabc7c5
Darren Redmond <Darren.Redmond@synchronoss.com>
2016-06-21 11:01:46 +0100

    update the tests for instrumentation

--- app/src/test/java/com/example/ksha0005/sample/MainActivityTest.java
+++ app/src/test/java/com/example/ksha0005/sample/MainActivityTest.java
@@ -1,38 +1,41 @@
 package com.example.ksha0005.sample;
 
-import android.widget.*;
-import android.test.ActivityInstrumentationTestCase2;
+import android.test.ActivityUnitTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-    public MainActivityTest(Class <MainActivity> activityClass) {
+public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
+
+    // Activity of the Target application
+    private MainActivity mainActivity;
+
+    public MainActivityTest() {
         super(MainActivity.class);
     }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+
+        // Getting a reference to the MainActivity of the target application
+        mainActivity = getActivity();
     }
 
 
     @SmallTest
     public void testEditText() {
-        EditText et = (EditText)getActivity().findViewById(R.id.usernameET);
+        final EditText et = (EditText) mainActivity.findViewById(R.id.usernameET);
         assertNotNull(et);
     }
 
     @SmallTest
-    public void testbutton() {
-        Button bt = (Button)getActivity().findViewById(R.id.loginBtn);
+    public void testButton() {
+        final Button bt = (Button) mainActivity.findViewById(R.id.loginBtn);
         assertNotNull(bt);
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
 }
